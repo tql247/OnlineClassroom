f090a0550815072b761d78a07e48bcf1ad944e01
tql247 <toitenlalinh9xpro@gmail.com>
2020-12-01 20:12:22 +0700

    invite student

--- service/inviteStudent.php
+++ service/inviteStudent.php
@@ -1,9 +1,12 @@
-<?php  
-	$student_id = $_POST["student_id"];
+<?php
+	require_once('../connection/connector.php');
+
+    $student_email = $_POST["student_email"];
 	$class_id = $_POST["class_id"];
-	// $feed_cover = $_POST["feed-cover"];
+    
+    $student_id = $conn->query("SELECT * from user WHERE `email` = '$student_email'")->fetch_assoc()["id"];
 	
-	$sql = "INSERT INTO feed (`student_id`, `class_id`) VALUES ('$student_id','$class_id') ";
+	$sql = "INSERT INTO user_class (`user_id`, `class_id`, `status`) VALUES ('$student_id','$class_id', 'c') ";
 	if($conn->query($sql) ===False){
 		die("ERROR:". $sql. $conn->error );
 	}else if (isset($_SERVER["HTTP_REFERER"])) {
